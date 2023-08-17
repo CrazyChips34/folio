77a938ae297339e5980412b5ba87314b375af511
Tiffany Pietersen <tiffanypietersen234@gmail.com>
2023-08-17 09:20:01 +0200

    editedAbout

--- folio/components/About.jsx
+++ folio/components/About.jsx
@@ -10,28 +10,11 @@ export const About = () => {
          <h2 className="py-5 text-4xl font-medium text-blue-500 md:text-5xl">
           About Me
           </h2>
-          <p className="max-w-2xl px-2 py-1 mx-auto text-sm text-gray-400 md:text-md">
-          Hi there! My name is Tiffany Pietersen, and I am a Full Stack Developer, with a passion for technology and finding innovative solutions to complex problems. 
-          I got my diploma in Software Development at IIE Rosebank College. 
-          It is my dad that helped spark my interest in going into the technology field, where he was very passionate about building and fixing machines particularly computers, and it ultimately led me to pursue it as a career.
-          <br/>
-          <br/>
-          In addition to my interest in being a Full Stack Developer, I have a variety of hobbies that keep me busy and help me approach problem-solving from a fresh perspective. 
-          I love spending time outdoors and exploring nature through hiking and playing soccer with friends. 
-          When I&#39;m not outside, I enjoy indulging in my love for e-sports, reading, and baking.
-          <br/>
-          <br/>
-          My curiosity and interest in technology go beyond my work as a developer. 
-          I am always seeking new ways to expand my knowledge and skills, whether it&#39;s through reading blogs or participating in online tutorials.
-          <br/>
-          <br/>
-          Currently, I am doing a learnership at CapaCiTi, which is helping me gain valuable real-world experience and allowing me to apply my technical skills in a professional setting. 
-          I am excited to continue learning and growing as a Full Stack Developer, and I am committed to using my skills to make a positive impact on people&aposs lives through software development.
-          </p>
+          
         
       </div>
       <div className="w-auto h-auto p-5 m-auto rounded-lg px-7">
-        <Image className="ease-in-out rounded-lg hover:animate-pulse"src={PortfolioImg} alt="ME" width={800} height={800} />{/*Image needs to stay the same size when screen gets smaller*/}
+        <Image className="ease-in-out rounded-lg hover:animate-pulse"src={PortfolioImg} alt="ME" width={800} height={800} />
       </div>
     </div>
   </div>
